528ef9391cf8ff4a7c16f389ecb3908dfda7b844
magnus <magnus@iastate.edu>
2022-08-07 22:14:30 -0500

    Working towards selecting term

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
-	let terms: Object[] = []
+	let terms: Object = []
 	let departments: Object[] = []
 	let selected_term = 1
 	let selected_department = "math"
 	let classes: string = ""
+	let data_pulled_for = {}
+	function term_names_to_term_ids(terms: any[]): Object {
+		let retval = Object.create(null)
+		terms.forEach((x) => {
+			let n: string = x.semesterTitle
+			let id: number = x.id
+			retval[n] = id
+		})
+		console.log(retval)
+		return retval
+	}
 	async function read_form_defaults() {
 		fetch(`http://127.0.0.1:8081/info/`, {
 			method: "GET",
 		})
 			.then((x) => x.json())
 			.then((x) => {
-				terms = x["semesters"]
+				terms = term_names_to_term_ids(x["semesters"])
 				departments = x["departments"]
-				console.log(terms)
 				console.log(departments)
+				console.log(terms)
 			})
 			.catch((err) => {
 				console.log(`error encountered: ${err}`)
@@ -24,9 +35,9 @@
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>
-	{terms}
-</p>
+{if terms is not undefined}
+
+{/if}
 <p>
 	{departments}
 </p>
