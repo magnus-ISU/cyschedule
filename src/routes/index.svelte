c8190fdf5b0ab61a63f5ee101a1123072a8fe296
magnus <magnus@iastate.edu>
2022-08-08 20:44:21 -0500

    Add interfaces to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,82 @@
 <script lang="ts">
-	let terms: any[] = []
-	let departments: Object[] = []
-	let selected_term = 1
-	let selected_department = "math"
-	let classes: string = ""
-	let data_pulled_for = {}
+	import Tab, { Label } from "@smui/tab"
+	import TabBar from "@smui/tab-bar"
+	import Textfield from '@smui/textfield';
+	import HelperText from '@smui/textfield/helper-text';
+
+	interface Term {
+		semesterTitle: string
+		id: number
+	}
+
+	interface Department {
+		id: number
+		title: string
+		abbreviation: string
+	}
+
+	interface SectionTime {
+		id: number
+		sectionSequenceNumber: number
+		instructionType: string
+		instrName: string
+		buildingName: string
+		roomNum: string
+		meetTimeDisplay: string
+		meetDaysDisplay: string
+	}
+
+	interface Section {
+		referenceNumber: string
+		openSeats: number
+		creditLow: number
+		creditHigh: number
+		startDateVal: string
+		stopDateVal: string
+		sectionTimes: SectionTime
+		partialSemesterComment: string
+		specialPermissionRequirementsDisplay: string
+		workshopFeeDisplay: string
+		secondaryDeliveryTypeDisplay: string
+		deliveryTypeDisplay: string
+		specialFeeTypeDisplay: string
+		offCampusDisplay: string
+		offCampusLocation: string
+		deliveryUrl: string
+		startDate: string
+		stopDate: string
+		courseId: number
+		sectionString: string
+	}
+
+	interface Classes {
+		id: number
+		classNumber: string
+		classTitle: string
+		classComments: string
+		classPreReqs: string
+		sections: Section[]
+	}
+
+	let terms: Term[]
+	let selected_term: Term
+	let departments: Department[]
+	let selected_department: Department
+	let classes_textbox: Record<number, string> = {}
+	let classes_pulled_for_departments: Record<string, Classes> = {}
+
 	async function read_form_defaults() {
 		fetch(`http://127.0.0.1:8081/info/`, {
 			method: "GET",
 		})
 			.then((x) => x.json())
 			.then((x) => {
-				terms = x["semesters"]
-				departments = x["departments"]
+				selected_term = x.semesters[0]
+				x.semesters.forEach((e: Term) => {
+					classes_textbox[e.id] = ""
+				});
+				terms = x.semesters
+				departments = x.departments
 				console.log(departments)
 				console.log(terms)
 			})
@@ -24,14 +88,19 @@
 	read_form_defaults()
 </script>
 
-<h1>Welcome to SvelteKit</h1>
 {#if terms != undefined}
-	{#each terms as term}
-		<p>
-			{term.semesterTitle}
-		</p>
-	{/each}
+	<TabBar tabs={terms} let:tab bind:active={selected_term}>
+		<Tab {tab}>
+			<Label>{tab.semesterTitle}</Label>
+		</Tab>
+	</TabBar>
+	<br/>
+	<Textfield
+		textarea
+		bind:value={classes_textbox[selected_term.id]}
+		label={"Classes for " + selected_term.semesterTitle}
+		input$rows={10}
+		input$cols={24}
+	>
+	</Textfield>
 {/if}
-<p>
-	{departments}
-</p>
